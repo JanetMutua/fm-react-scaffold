bd311f57dd9f900e124cf5101f3466807c0df725
JanetMutua <janetmutua04@gmail.com>
2025-10-03 17:44:48 +0300

    vite.config.js

--- 01-huddle-landing-page/vite.config.js
+++ 01-huddle-landing-page/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fm-react-scaffold/01-huddle-landing-page/",
   plugins: [react()],
 });
