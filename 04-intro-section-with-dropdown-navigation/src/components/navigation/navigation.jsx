6f3a44cc9df36fcc06f6f240c62eb599c4f48456
JanetMutua <janetmutua04@gmail.com>
2025-11-17 23:59:57 +0300

    updates

---
+++ 04-intro-section-with-dropdown-navigation/src/components/navigation/navigation.jsx
@@ -0,0 +1,17 @@
+// import pageLogo from "../../../images/logo.svg";
+
+export function Navigation() {
+  return (
+    <>
+      <header>
+        {/* <img src={pageLogo} alt="snap logo" /> */}
+        <ul>
+          <li>Features</li>
+          <li>Company</li>
+          <li>Careers</li>
+          <li>About</li>
+        </ul>
+      </header>
+    </>
+  );
+}
